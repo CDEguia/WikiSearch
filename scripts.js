a805d4a389e57c2e4acd6b7b5882a0d5d041c0a7
Candelario Daniel Eguia <candelario.eguia@gmail.com>
2017-07-04 13:19:37 -0700

    fix bug with ifram display

--- scripts.js
+++ scripts.js
@@ -1,10 +1,15 @@
-/**
- * Created by cdeguia on 6/5/16.
+/*
+ *  Created by C. Daniel Eguia on 6/5/16.
  */
 
-var winResize = function () {
-    document.getElementById('frame').style.height = (window.innerHeight-100) + 'px';
+function winResize() {
+  $('#home').height(window.innerHeight);
+  $('#connect').height(window.innerHeight);
+  $('#frame').height(window.innerHeight - 90);
 }
+$(document).ready(function () {
+  winResize();
+});
 
 $(window).ready(function() {
 
@@ -12,28 +17,28 @@ $(window).ready(function() {
         winResize();
     };
 
-    $('.action').on("click", event, aaction)
     $(function () {
         $("#searchbox").focus();
     });
-    function aaction(event) {
+
+    function aAction(event) {
         event.preventDefault();
         var a = $(this).attr('href');
         document.getElementById("resultsWindow").innerHTML = '<iframe id="frame" src="' + a + '" width= 100%></iframe>';
         winResize();
     };
+   
+    $(".randomBtn").on("click", aAction);
 
-    $("form").submit(function () {
+    $("form").submit(function (event) {
         event.preventDefault();
         var searchString = $(".searchString").val();
-        //console.log(searchString);
 
         $.getJSON('http://en.wikipedia.org/w/api.php?action=opensearch&format=json&limit=13&warningsaserror=1&callback=?&search=' + searchString, function (data) {
-            // console.log(data);
-            //$("#resultsWindow").empty();
+
             var out = "";
             $.each(data[1], function (i, val) {
-                // console.log(val.title);
+                
                 out += '<a href="' + data[3][i] + '"  class="action">';
                 out += '<div class="panel panel-info"><div class="panel-heading">'
                 out += '<h3 class="panel-title">' + val + '</h3></div>';
@@ -41,7 +46,7 @@ $(window).ready(function() {
                 out += '</a>';
             });
             document.getElementById("resultsWindow").innerHTML = out;
-            $('.action').on("click", event, aaction)
+            $('.action').on("click", event, aAction)
         });
 
     });
